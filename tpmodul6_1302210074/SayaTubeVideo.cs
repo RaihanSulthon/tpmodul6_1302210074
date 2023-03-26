b8aa9fad9d224dfb5198f8236ed113b4179046b5
RaihanSulthon <raihansy@student.telkomuniversity.ac.id>
2023-03-26 19:52:42 +0700

    menambahkan class SayaTubeVideo

---
+++ tpmodul6_1302210074/SayaTubeVideo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302210074
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private String title;
+        private int playCount;
+
+        public SayaTubeVideo(String title)
+        {
+            this.title = title;
+            var a = this.title.Substring(0, Math.Min(100, this.title.Length));
+            Contract.Requires(this.title != null);
+
+            String Digit = "";
+
+            Random random= new Random();
+            id= random.Next(1, 99999);
+            Digit += id.ToString("Digit 5");
+
+            playCount = 0;
+        }
+
+        public void PrintVideoDetails()
+        {
+
+        }
+
+        public void IncreasePlayCount(int x)
+        {
+
+        }
+    }
+}
